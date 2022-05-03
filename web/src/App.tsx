0b8e0153cd2db372ca20f4ccee3fea8e266f3088
Marcos Paranhos <mark.paranhoss@gmail.com>
2022-05-03 18:44:58 -0300

    Feedback button

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,7 +1,25 @@
-import { useState } from 'react'
-
-function App() {
-  return <h1>Hello World</h1>
+/* interface ButtonProps {
+  text?: string;
 }
 
-export default App
+function Button(props: ButtonProps) {
+  return <button className="bg-violet-600 px-4 h-10 rounded hover:bg-violet-800 transition-colors">{props.text ?? 'Default'}</button>
+}
+
+function App() {
+  return (
+    <div className="flex gap-2">
+      <Button text="Enviar" />
+      <Button text="Ok" />
+      <Button />
+    </div>
+  )
+}
+
+export default App */
+
+import { Widget } from "./components/Widget";
+
+export function App() {
+  return <Widget />
+}
